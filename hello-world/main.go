fc7d0fe774bb2280aa6dc1045c12a3072fd15ddd
toriumi <saori.toriumi.54@gmail.com>
2019-05-13 17:33:51 +0900

    Fix property

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,51 +1,36 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"io/ioutil"
-	"net/http"
+	"encoding/json"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-var (
-	// DefaultHTTPGetAddress Default Address
-	DefaultHTTPGetAddress = "https://checkip.amazonaws.com"
-
-	// ErrNoIP No IP found in response
-	ErrNoIP = errors.New("No IP in HTTP response")
-
-	// ErrNon200Response non 200 status code in response
-	ErrNon200Response = errors.New("Non 200 Response found")
-)
-
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	resp, err := http.Get(DefaultHTTPGetAddress)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
+	personID := request.PathParameters["personID"]
+	personName := request.QueryStringParameters["personName"]
+	old := 23
 
-	if resp.StatusCode != 200 {
-		return events.APIGatewayProxyResponse{}, ErrNon200Response
+	person := PersonResponse{
+		PersonID:   personID,
+		PersonName: personName,
+		Old:        old,
 	}
-
-	ip, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
-	if len(ip) == 0 {
-		return events.APIGatewayProxyResponse{}, ErrNoIP
-	}
+	jsonBytes, _ := json.Marshal(person)
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("Hello, %v", string(ip)),
+		Body:       string(jsonBytes),
 		StatusCode: 200,
 	}, nil
 }
 
 func main() {
 	lambda.Start(handler)
+}
+
+type PersonResponse struct {
+	PersonID   string `json:"personID"`
+	PersonName string `json:"personName"`
+	Old        int    `json:"old"`
 }
